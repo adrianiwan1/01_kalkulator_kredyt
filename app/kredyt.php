e5dc48e6a2747ed2142469300a260ea782924298
adrianiwan1 <adrianiwan1@gmail.com>
2022-03-01 23:10:12 +0100

    upload controller

---
+++ app/kredyt.php
@@ -0,0 +1,33 @@
+<?php
+require_once dirname(__FILE__).'/../config.php';
+
+$kw = $_REQUEST ['kw'];
+$lat = $_REQUEST ['lat'];
+$procent = $_REQUEST ['procent'];
+$result="";
+
+
+if ( ! (isset($kw) && isset($lat) && isset($procent))) {
+	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+}
+if ( $kw == "") {
+	$messages [] = 'Nie podano kwoty kredytu';
+}
+if ( $lat == "") {
+	$messages [] = 'Nie podano ilosci lat';
+}
+if ( $procent == "") {
+	$messages [] = 'Nie podano procentu porzyczki';
+}
+if (empty( $messages )) { 
+
+    $kw = floatval($kw);
+    $lat = intval($lat);
+    $procent = intval($procent);
+
+    $pr= $procent * 0.01;
+    $calosc = ($kw * $pr) + $kw;
+    $result = strval(round($calosc/($lat * 12),2));
+}
+include 'kredyt_view.php';
+
