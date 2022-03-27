ec2453160df9a89c6ee7162b02854be261b3f17c
Adrian Iwañski <adriadniwan1@gmail.com>
2022-03-27 16:45:51 +0200

    redesighn app

--- app/kredyt.php
+++ app/kredyt.php
@@ -1,4 +1,18 @@
 <?php
+
+//Skrypt uruchamiający akcję wykonania obliczeń kalkulatora
+// - należy zwrócić uwagę jak znacząco jego rola uległa zmianie
+//   po wstawieniu funkcjonalności do klasy kontrolera
+
+require_once dirname(__FILE__).'/../config.php';
+
+//załaduj kontroler
+require_once $conf->root_path.'/app/KredytCtrl.class.php';
+
+//utwórz obiekt i użyj
+$ctrl = new KredytCtrl();
+$ctrl->process();
+/*
 require_once dirname(__FILE__) . '/../config.php';
 
 include _ROOT_PATH . '/app/security/check.php';
@@ -72,3 +86,4 @@ if(validate($kw, $lat, $procent,$messages))
     
 }
 include _ROOT_PATH .'/app/kredyt_view.php';
+*/
--- index.php
+++ index.php
@@ -1,6 +1,15 @@
-
 <?php
+/*
 require_once dirname(__FILE__).'/config.php';
 include _ROOT_PATH.'/app/kredyt.php';
+*/
+
+require_once dirname(__FILE__).'/config.php';
+
+//przekierowanie przeglądarki klienta (redirect)
+//header("Location: ".$conf->root_path."/app/calc.php");
+
+//przekazanie żądania do następnego dokumentu ("forward")
+include $conf->root_path.'/app/kredyt.php';
 ?>
 
