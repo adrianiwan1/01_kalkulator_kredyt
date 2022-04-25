80e2bd714472653d65dd1c00501b67964489d510
Adrian Iwañski <adriadniwan1@gmail.com>
2022-04-25 22:09:23 +0200

    Routing

--- ctrl.php
+++ ctrl.php
@@ -9,38 +9,9 @@ require_once 'init.php';
 // Wystarczy już tylko podjąć decyzję co zrobić na podstawie $action.
 
 // Dodatkowo zmieniono organizację kontrolerów i widoków. Teraz wszystkie są w odpowiednio nazwanych folderach w app
-/*
-switch ($action) {
-	default : // 'calcView'
-	    // załaduj definicję kontrolera
-		
-		// utwórz obiekt i uzyj
-		include 'check.php';
-		$ctrl = new app\controllers\KredytCtrl();
-		$ctrl->generateView ();
-	break;
-	case 'login' :
-		
-		$ctrl = new app\controllers\LoginCtrl();
-		$ctrl->doLogin();
-	break;
-	case 'KredytCompute' :
-		// załaduj definicję kontrolera
-		
-		// utwórz obiekt i uzyj
-		
-		$ctrl = new app\controllers\KredytCtrl();
-		$ctrl->process ();
-	break;
-	case 'logout' :
-		include 'check.php';  // KONTROLA
-		$ctrl = new app\controllers\LoginCtrl();
-		$ctrl->doLogout();
-	break;
-}
-*/
-getConf()->login_action = 'login';
 
+getConf()->login_action = 'login';
+/*
 switch ($action) {
 	default :
 		control('app\\controllers', 'KredytCtrl',		'generateView', ['user','admin']);
@@ -52,3 +23,13 @@ switch ($action) {
 	case 'logout' : 
 		control(null, 'LoginCtrl',	'doLogout',		['user','admin']);
 }
+*/
+getRouter()->setDefaultRoute('KredytShow'); // akcja/ścieżka domyślna
+getRouter()->setLoginRoute('login'); // akcja/ścieżka na potrzeby logowania (przekierowanie, gdy nie ma dostępu)
+
+getRouter()->addRoute('KredytShow',    'KredytCtrl',  ['user','admin']);
+getRouter()->addRoute('KredytCompute', 'KredytCtrl',  ['user','admin']);
+getRouter()->addRoute('login',       'LoginCtrl');
+getRouter()->addRoute('logout',      'LoginCtrl', ['user','admin']);
+
+getRouter()->go();
